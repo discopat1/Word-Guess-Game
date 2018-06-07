a21e0b55e41da4f15cde939441645e1944c107be
Patrick Neff <patrickneff@Patricks-MacBook-Air.local>
2018-06-06 18:26:05 -0600

    different version turning each word into an array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,34 @@
 
 // variables
-
+var startGame
+var blankWord
 
 
 // create event for game to start upon key press
-function startGame() {
-    myGameArea.start();
-}
+// document.onkeyup = function() {
+//     var startGame = String.fromCharCode(event.keyCode);
+// }
+// console.log(startGame);
 
 // create a word bank
 
-var wordBank = ["cowboy", "revolver", "wagon", "rattlesnake", "saloon", "sasparilla", "stirup", "railroad"];
+var wordBank = {
+    cowboy: ["c", "o", "w", "b", "o", "y"],
+    revolver: ["r", "e", "v", "o", "l", "v", "e", "r"],
+    wagon: ["w", "a", "g", "o", "n"],
+    rattlesnake: ["r", "a", "t", "t", "l", "e", "s", "n", "a", "k", "e"],
+    saloon: ["s", "a", "l", "o", "o", "n"],
+    sasparilla: ["s", "a", "s", "p", "a", "r", "i", "l", "l", "a"],
+    stirup: ["s","t", "i", "r", "u", "p"],
+    railroad: ["r", "a", "i", "l", "r", "o", "a", "d"]
+}
 
-console.log(wordBank);
+console.log(wordBank.cowboy);
 
 // computer randomly picks a word from the word bank
 
-var targetWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+// var targetWord = wordBank[Math.floor(Math.random()*wordBank)];
+
 
 console.log(targetWord);
 
@@ -24,54 +36,61 @@ console.log(targetWord);
 
 //something like this maybe
 
-// var targetWord = wordBank[0];
-if (targetWord === "cowboy") {
-   var missingWord = "_ _ _ _ _ _"
-}
 
-if (targetWord === "revolver") {
-    var missingWord = "_ _ _ _ _ _ _ _"
- }
+// function blank() {
+//     document.querySelector('#missing-word').innerHTML = blankWord;
 
- if (targetWord === "wagon") {
-    var missingWord = "_ _ _ _ _"
- }
+
+// if (targetWord === "cowboy") {
+//    blankWord = "_ _ _ _ _ _"
+// }
+
+// if (targetWord === "revolver") {
+//     blankWord = "_ _ _ _ _ _ _ _"
+//  }
+
+//  if (targetWord === "wagon") {
+//     blankWord = "_ _ _ _ _"
+//  }
  
- if (targetWord === "rattlesnake") {
-     var missingWord = "_ _ _ _ _ _ _ _ _ _ _"
-  }
+//  if (targetWord === "rattlesnake") {
+//     blankWord = "_ _ _ _ _ _ _ _ _ _ _"
+//   }
 
-  if (targetWord === "saloon") {
-    var missingWord = "_ _ _ _ _ _"
- }
+//   if (targetWord === "saloon") {
+//     blankWord = "_ _ _ _ _ _"
+//  }
  
- if (targetWord === "sasparilla") {
-     var missingWord = "_ _ _ _ _ _ _ _ _ _"
-  }
+//  if (targetWord === "sasparilla") {
+//     blankWord = "_ _ _ _ _ _ _ _ _ _"
+//   }
 
-  if (targetWord === "stirup") {
-    var missingWord = "_ _ _ _ _ _"
- }
+//   if (targetWord === "stirup") {
+//     blankWord = "_ _ _ _ _ _"
+//  }
  
- if (targetWord === "railroad") {
-     var missingWord = "_ _ _ _ _ _ _ _"
-  }
+//  if (targetWord === "railroad") {
+//     blankWord = "_ _ _ _ _ _ _ _"
+//   }
+  
+// }
+  
 
- 
+  
+//dostuff();
+// console.log(blankWord);
 
-console.log(missingWord);
+
 
 // blank word is presented
 
-// function blank() {
-//     document.querySelector('#missing-word').innerHTML = missingWord;
-// }
+
 
 // key press is captured
 
-document.onkeyup = function() {
-    var userPick = String.fromCharCode(event.keyCode);
-}
+// document.onkeyup = function() {
+    // var userPick = String.fromCharCode(event.keyCode);
+// }
 
 // condition: if letter matches current word is displayed on "current word"
 
@@ -92,5 +111,4 @@ var wins = 0;
 
 // if word is matched add 1 to wins
 
-
-
+// turn wordBank into an object and turn the words into arrays
